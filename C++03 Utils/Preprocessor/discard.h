aa7183aa3b6aa45f49f64d7e8b2fec696e778982
Chris DeVisser <chrisndevisser@gmail.com>
2015-02-21 14:02:55 -0500

    Fix discard

--- C++03 Utils/Preprocessor/discard.h
+++ C++03 Utils/Preprocessor/discard.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define UW_DISCARD1(_) 
-#define UW_DISCARD2(_, _)
-#define UW_DISCARD3(_, _, _)
+#define UW_DISCARD1(arg1) 
+#define UW_DISCARD2(arg1, arg2)
+#define UW_DISCARD3(arg1, arg2, arg3)
