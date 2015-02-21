57aff34568894ceb470d7fc871f1f6fd3879aeb8
Chris DeVisser <chrisndevisser@gmail.com>
2015-02-21 16:20:27 -0500

    Change Preprocessor->if to use Preprocessor->bool; now works with arguments up to 16

--- C++03 Utils/Preprocessor/if.h
+++ C++03 Utils/Preprocessor/if.h
@@ -1,14 +1,12 @@
 #pragma once
 
+#include "bool.h"
 #include "concat.h"
 
-//We will delegate to the implementation macro so that conditionInteger is expanded if it is a macro.
-#define UW_IF(conditionInteger, trueReplacement, falseReplacement) IMPL_UW_IF(conditionInteger, trueReplacement, falseReplacement)
-
-//We will use two helper macros to determine whether conditionInteger is 1 or 0.
+//We will use two helper macros to determine whether conditionInteger is effectively 1 or 0.
 //We concatenate a common prefix with the value.
 //Below are two macros representing the two possible results.
-#define IMPL_UW_IF(conditionInteger, trueReplacement, falseReplacement) UW_CONCAT(IMPL_UW_IF_, conditionInteger)(trueReplacement, falseReplacement)
+#define UW_IF(conditionInteger, trueReplacement, falseReplacement) UW_CONCAT(IMPL_UW_IF_, UW_BOOL(conditionInteger))(trueReplacement, falseReplacement)
 
 #define IMPL_UW_IF_1(trueReplacement, falseReplacement) trueReplacement
 #define IMPL_UW_IF_0(trueReplacement, falseReplacement) falseReplacement
